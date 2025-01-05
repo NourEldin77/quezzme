9731cc849148e2a3537e43d68bb436cf38547c07
NourEldin77 <noureldean.2003@gmail.com>
2025-01-05 07:28:20 +0200

    feat: user authentication functionality

---
+++ src/controllers/authController.ts
@@ -0,0 +1,56 @@
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import prisma from "../../db";
+
+const generateJWT = (userId: string) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET!, {
+    expiresIn: "1h",
+  });
+};
+
+export const register = async (req: Request, res: Response) => {
+  const { username, email, password, passwordConfirmation } = req.body;
+
+  if (password !== passwordConfirmation) {
+    return res.status(400).json({ error: "Passwords do not match" });
+  }
+  try {
+    const hashedPassword = await bcrypt.hash(
+      password,
+      process.env.SALT_ROUNDS!
+    );
+
+    const user = await prisma.user.create({
+      data: { username, email, password: hashedPassword },
+    });
+
+    const token = generateJWT(user.id);
+    res.status(201).json({ token, user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to register user" });
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    const token = generateJWT(user.id);
+    res.json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to log in" });
+  }
+};

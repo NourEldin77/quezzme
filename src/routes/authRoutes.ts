651a7e7546487f0b0441d1be622e0008e823cb98
NourEldin77 <noureldean.2003@gmail.com>
2025-01-07 06:28:57 +0200

    feat: add validation middleware to user route

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { register, login } from "../controllers/authController";
+import {
+  validate,
+  handleInputErrors,
+} from "../middlewares/validationMiddleware";
 
 const router = Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", validate("createUser"), handleInputErrors, register);
+router.post("/login", validate("loginUser"), handleInputErrors, login);
 
 export default router;

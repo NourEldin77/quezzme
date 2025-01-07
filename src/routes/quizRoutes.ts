5d6d515c1728c42d45473e97ea06e81b50a1fc9b
NourEldin77 <noureldean.2003@gmail.com>
2025-01-07 06:29:36 +0200

    feat: add validation middleware to quiz routes

--- src/routes/quizRoutes.ts
+++ src/routes/quizRoutes.ts
@@ -3,26 +3,50 @@ import { protect } from "../middlewares/authMiddleware";
 import * as quizController from "../controllers/quizController";
 import * as questionController from "../controllers/questionController";
 import * as answerController from "../controllers/answerController";
+import {
+  validate,
+  handleInputErrors,
+} from "../middlewares/validationMiddleware";
 
 const router = Router();
 
 /* quizz routes */
 
-router.post("/", quizController.createQuiz); // Create a quiz
+router.post(
+  "/",
+  validate("createQuiz"),
+  handleInputErrors,
+  quizController.createQuiz
+); // Create a quiz
 router.get("/", quizController.getAllQuizzes); // Get all user's quizzes
 router.get("/:id", quizController.getQuizById); // Get a quiz by id
-router.put("/:id", quizController.updateQuiz); // Update a quiz by id
+router.put(
+  "/:id",
+  validate("updateQuiz"),
+  handleInputErrors,
+  quizController.updateQuiz
+); // Update a quiz by id
 router.delete("/:id", quizController.deleteQuiz); // Delete a quiz by id
 
 /* Question routes */
 
-router.post("/:quizId/questions", questionController.addQuestionToQuiz); // Create a question and add it to a quiz
+router.post(
+  "/:quizId/questions",
+  validate("createQuestion"),
+  handleInputErrors,
+  questionController.addQuestionToQuiz
+); // Create a question and add it to a quiz
 router.get("/:quizId/questions", questionController.getAllQuestionsForQuiz); // get all questions from a quiz
 router.get(
   "/:quizId/questions/:questionId",
   questionController.getQuestionById
 ); // get a question by id
-router.put("/:quizId/questions/:questionId", questionController.updateQuestion); // update a question by id
+router.put(
+  "/:quizId/questions/:questionId",
+  validate("updateQuestion"),
+  handleInputErrors,
+  questionController.updateQuestion
+); // update a question by id
 router.delete(
   "/:quizId/questions/:questionId",
   questionController.deleteQuestion
@@ -32,7 +56,9 @@ router.delete(
 
 router.post(
   "/:quizId/questions/:questionId/answers",
-  answerController.addAnswerToQuestion
+  answerController.addAnswerToQuestion,
+  validate("createAnswer"),
+  handleInputErrors
 ); // Create an answer and add it to a question
 router.get(
   "/:quizId/questions/:questionId/answers",
@@ -44,7 +70,9 @@ router.get(
 ); // get an answer by id
 router.put(
   "/:quizId/questions/:questionId/answers/:answerId",
-  answerController.updateAnswer
+  answerController.updateAnswer,
+  validate("updateAnswer"),
+  handleInputErrors
 ); // update an answer by id
 router.delete(
   "/:quizId/questions/:questionId/answers/:answerId",
